6552be6f23411df758e796a984ea4df0418d161a
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2023-02-24 11:34:35 +0200

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace podgotovka
+{
+    class Animal:IComparable<Animal>
+    {
+        public Animal(string name, int age, params string[] children)
+        {
+            Name = name;
+            Age = age;
+            Children = children.ToList();
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> Children { get; set; }
+
+        public int CompareTo(Animal other)
+        {
+            int result = other.Age.CompareTo(this.Age);
+            if (result==0)
+            {
+                result = this.Name.CompareTo(other.Name);
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Age}";
+        }
+
+    }
+}
---
+++ AnimalComparator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace podgotovka
+{
+    class AnimalComparator : IComparer<Animal>
+    {
+        public List<Animal> animals = new List<Animal>();
+
+        public AnimalComparator(List<Animal> animals)
+        {
+            this.animals = animals;
+        }
+
+        public int Compare(Animal x, Animal y)
+        {
+            int result = x.Name.CompareTo(y.Name);
+            if (result == 0)
+            {
+                result = y.Age.CompareTo(x.Age);
+            }
+            return result;
+        }
+    }
+}
