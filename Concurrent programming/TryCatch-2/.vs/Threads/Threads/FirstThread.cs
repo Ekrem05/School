7f81be42cfb4882dee12a356e9d3bdeafb1bbc57
Ekrem05 <ekremnptg@gmail.com>
2023-02-27 20:30:45 +0200

    100

---
+++ Concurrent programming/TryCatch-2/.vs/Threads/Threads/FirstThread.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    public class FirstThread
+    {
+        private string[] Foods { get; set; }
+        private string[] Beverages { get; set; }
+        public FirstThread()
+        {
+            Beverages = new string[10];
+            Foods = new string[10];
+        }
+        public void DoTask1()
+        {
+            string[] input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToArray();
+            for (int i = 0; i < input.Length; i++)
+            {
+                Foods[i] = input[i];
+            }
+
+        }
+        public void DoTask2()
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToArray();
+            for (int i = 0; i < input.Length; i++)
+            {
+                Beverages[i] = input[i];
+            }
+
+        }
+        public  void Print()
+        {
+            StringBuilder sb=new StringBuilder();
+            for (int i = 0; i < Foods.Length; i++)
+            {
+                sb.AppendLine(Foods[i] + " ");
+            }
+            sb.AppendLine();
+            for (int i = 0; i < Beverages.Length; i++)
+            {
+                sb.AppendLine(Beverages[i] + " ");
+            }
+            Console.WriteLine(   sb.ToString().TrimEnd());
+        }
+    }
+}
---
+++ Concurrent programming/TryCatch-2/.vs/Threads/Threads/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Threads
+{
+    using System.Threading;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstThread first = new();
+            Thread thread1 = new(first.DoTask1);
+            Thread thread2 = new(first.DoTask2);
+            Thread thread3 = new(first.Print);
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+    }
+}
